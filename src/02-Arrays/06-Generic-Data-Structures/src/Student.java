c95ff1fb2b69bd144ca665cdd7e361521e4b6689
bianxinhuan <bianxinhuan@gmail.com>
2019-04-15 21:52:42 +0800

    2-6 使用泛型

---
+++ src/02-Arrays/06-Generic-Data-Structures/src/Student.java
@@ -0,0 +1,30 @@
+/**
+ * -
+ *
+ * @author bianxinhuan
+ * @date 2019-04-15 21:33:51
+ */
+public class Student {
+
+    private String name;
+    private int score;
+
+    public Student(String name, int score) {
+        this.name = name;
+        this.score = score;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Student(name: %s, score: %d)", name, score);
+    }
+
+    public static void main(String[] args) {
+
+        Array<Student> arr = new Array<>();
+        arr.addLast(new Student("Alice", 100));
+        arr.addLast(new Student("Bob", 66));
+        arr.addLast(new Student("Charlie", 88));
+        System.out.println(arr);
+    }
+}

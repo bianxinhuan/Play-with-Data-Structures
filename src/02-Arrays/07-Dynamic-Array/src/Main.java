c73bbfbb56a5ac9ebb1c1c8bfac6c7b8b50f56d2
bianxinhuan <bianxinhuan@gmail.com>
2019-04-15 22:35:41 +0800

    2-7 动态数组

--- src/02-Arrays/07-Dynamic-Array/src/Main.java
+++ src/02-Arrays/07-Dynamic-Array/src/Main.java
@@ -7,7 +7,7 @@
 public class Main {
 
     public static void main(String[] args) {
-        Array<Integer> arr = new Array<>(20);
+        Array<Integer> arr = new Array<>();
         for (int i = 0; i < 10; i++) {
             arr.addLast(i);
         }
@@ -18,7 +18,6 @@ public class Main {
 
         arr.addFirst(-1);
         System.out.println(arr);
-        // [-1, 0, 100, 1, 2, 3, 4, 5, 6, 7, 8, 9]
 
         arr.remove(2);
         System.out.println(arr);

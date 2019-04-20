31dc507eadb0c349c5ec233f13d91dc8fd55185c
bianxinhuan <bianxinhuan@gmail.com>
2019-04-20 22:33:31 +0800

    3-6 循环队列

---
+++ src/03-Stacks-and-Queues/06-Loop-Queue/src/ArrayQueue.java
@@ -0,0 +1,75 @@
+/**
+ * 动态数组实现的队列
+ *
+ * @author bianxinhuan
+ * @date 2019-04-19 21:00:21
+ */
+public class ArrayQueue<E> implements Queue<E> {
+
+    private Array<E> array;
+
+    public ArrayQueue(int capacity) {
+        this.array = new Array<>(capacity);
+    }
+
+    public ArrayQueue() {
+        this.array = new Array<>();
+    }
+
+    @Override
+    public int getSize() {
+        return array.getSize();
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return array.isEmpty();
+    }
+
+    public int getCapacity() {
+        return array.getCapacity();
+    }
+
+    @Override
+    public void enqueue(E e) {
+        array.addLast(e);
+    }
+
+    @Override
+    public E dequeue() {
+        return array.removeFirst();
+    }
+
+    @Override
+    public E getFront() {
+        return array.getFirst();
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder res = new StringBuilder();
+        res.append("Queue: ");
+        res.append("front [");
+        for (int i = 0; i < array.getSize(); i++) {
+            res.append(array.get(i));
+            if (i != array.getSize() - 1) {
+                res.append(", ");
+            }
+        }
+        res.append("] tail");
+        return res.toString();
+    }
+
+    public static void main(String[] args) {
+
+        ArrayQueue<Integer> queue = new ArrayQueue<>();
+        for (int i = 0; i < 10; i++) {
+            queue.enqueue(i);
+            System.out.println(queue);
+            if (i % 3 == 2) {
+                queue.dequeue();
+                System.out.println(queue);
+            }
+        }
+    }
+}
---
+++ src/03-Stacks-and-Queues/06-Loop-Queue/src/LoopQueue.java
@@ -0,0 +1,56 @@
+import org.omg.CORBA.Object;
+
+/**
+ * -
+ *
+ * @author bianxinhuan
+ * @date 2019-04-20 22:21:15
+ */
+public class LoopQueue<E> implements Queue<E> {
+
+    private E[] data;
+    private int front, tail;
+    private int size;
+
+    public LoopQueue(int capacity) {
+        data = (E[]) new Object[capacity + 1];
+        front = 0;
+        tail = 0;
+        size = 0;
+    }
+
+    public LoopQueue() {
+        this(10);
+    }
+
+    public int getCapacity() {
+        return data.length - 1;
+    }
+
+    @Override
+    public int getSize() {
+        return size;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return front == tail;
+    }
+
+    // 下一小节再做具体实现
+    @Override
+    public void enqueue(E e) {
+    }
+
+    // 下一小节再做具体实现
+    @Override
+    public E dequeue() {
+        return null;
+    }
+
+    // 下一小节再做具体实现
+    @Override
+    public E getFront() {
+        return null;
+    }
+}

14656cd0acd9035e35c65b24e2fd5b0d7f35b933
bianxinhuan <bianxinhuan@gmail.com>
2019-04-22 21:53:15 +0800

    4-1 什么是链表

---
+++ src/04-Linked-List/01-Linked-List-Basics/src/LinkedList.java
@@ -0,0 +1,32 @@
+/**
+ * -
+ *
+ * @author bianxinhuan
+ * @date 2019-04-22 14:09:19
+ */
+public class LinkedList<E> {
+
+    private class Node {
+        public E e;
+        public Node next;
+
+        public Node(E e, Node next) {
+            this.e = e;
+            this.next = next;
+        }
+
+        public Node(E e) {
+            this(e, null);
+        }
+
+        public Node() {
+            this(null, null);
+        }
+
+        @Override
+        public String toString() {
+            return e.toString();
+        }
+    }
+
+}
---
+++ src/04-Linked-List/01-Linked-List-Basics/src/Main.java
@@ -0,0 +1,8 @@
+/**
+ * -
+ *
+ * @author bianxinhuan
+ * @date 2019-04-22 14:09:21
+ */
+public class Main {
+}

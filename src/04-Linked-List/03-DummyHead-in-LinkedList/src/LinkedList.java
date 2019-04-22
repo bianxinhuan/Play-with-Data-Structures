bbba0208685ca9915a712369045580e0df5b349d
bianxinhuan <bianxinhuan@gmail.com>
2019-04-22 22:13:46 +0800

    4-3 使用链表的虚拟头结点

---
+++ src/04-Linked-List/03-DummyHead-in-LinkedList/src/LinkedList.java
@@ -0,0 +1,96 @@
+/**
+ * -
+ *
+ * @author bianxinhuan
+ * @date 2019-04-22 14:09:19
+ */
+public class LinkedList<E> {
+
+    private class Node {
+        public E e;
+        public Node next;
+
+        public Node(E e, Node next) {
+            this.e = e;
+            this.next = next;
+        }
+
+        public Node(E e) {
+            this(e, null);
+        }
+
+        public Node() {
+            this(null, null);
+        }
+
+        @Override
+        public String toString() {
+            return e.toString();
+        }
+    }
+
+    private Node dummyHead;
+    private int size;
+
+    public LinkedList() {
+        dummyHead = new Node();
+    }
+
+    /**
+     * 获取链表中的元素个数
+     *
+     * @return
+     */
+    public int getSize() {
+        return size;
+    }
+
+    /**
+     * 返回链表是否为空
+     *
+     * @return
+     */
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    /**
+     * 在链表的index(0-based)位置添加新的元素e
+     * 在链表中不是一个常用的操作，练习用：）
+     *
+     * @param index
+     * @param e
+     */
+    public void add(int index, E e) {
+        if (index < 0 || index > size) {
+            throw new IllegalArgumentException("Add failed. Illegal index.");
+        }
+
+        Node prev = dummyHead;
+        for (int i = 0; i < index; i++) {
+            prev = prev.next;
+        }
+
+        prev.next = new Node(e, prev.next);
+        size++;
+    }
+
+    /**
+     * 在链表头添加新的元素e
+     *
+     * @param e
+     */
+    public void addFirst(E e) {
+        add(0, e);
+    }
+
+    /**
+     * 在链表末尾添加新的元素e
+     *
+     * @param e
+     */
+    public void addLast(E e) {
+        add(size, e);
+    }
+
+}

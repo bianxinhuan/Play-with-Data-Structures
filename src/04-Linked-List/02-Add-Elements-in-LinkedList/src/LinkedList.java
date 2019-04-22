ac655c3f6504616ac0b2413327b66262b976bbcb
bianxinhuan <bianxinhuan@gmail.com>
2019-04-22 21:56:01 +0800

    4-2 在链表中添加元素

---
+++ src/04-Linked-List/02-Add-Elements-in-LinkedList/src/LinkedList.java
@@ -0,0 +1,105 @@
+/**
+ * -
+ *
+ * @author bianxinhuan
+ * @date 2019-04-22 14:09:19
+ */
+public class LinkedList<E> {
+
+    private class Node {
+        public E e;
+        public Node next;
+
+        public Node(E e, Node next) {
+            this.e = e;
+            this.next = next;
+        }
+
+        public Node(E e) {
+            this(e, null);
+        }
+
+        public Node() {
+            this(null, null);
+        }
+
+        @Override
+        public String toString() {
+            return e.toString();
+        }
+    }
+
+    private Node head;
+    private int size;
+
+    /**
+     * 获取链表中的元素个数
+     *
+     * @return
+     */
+    public int getSize() {
+        return size;
+    }
+
+    /**
+     * 返回链表是否为空
+     *
+     * @return
+     */
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    /**
+     * 在链表头添加新的元素e
+     *
+     * @param e
+     */
+    public void addFirst(E e) {
+
+//        Node node = new Node(e);
+//        node.next = head;
+//        head = node;
+
+        head = new Node(e, head);
+
+        size++;
+    }
+
+    /**
+     * 在链表的index(0-based)位置添加新的元素e
+     * 在链表中不是一个常用的操作，练习用：）
+     *
+     * @param index
+     * @param e
+     */
+    public void add(int index, E e) {
+
+        if (index == 0) {
+            addFirst(e);
+        } else {
+            Node prev = head;
+            for (int i = 0; i < index - 1; i++) {
+                prev = prev.next;
+            }
+
+//            Node node = new Node(e);
+//            node.next = prev.next;
+//            prev.next = node;
+
+            prev.next = new Node(e, prev.next);
+
+            size++;
+        }
+    }
+
+    /**
+     * 在链表末尾添加新的元素e
+     *
+     * @param e
+     */
+    public void addLast(E e) {
+        add(size, e);
+    }
+
+}
---
+++ src/04-Linked-List/02-Add-Elements-in-LinkedList/src/Main.java
@@ -0,0 +1,19 @@
+/**
+ * -
+ *
+ * @author bianxinhuan
+ * @date 2019-04-22 14:09:21
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        LinkedList<Integer> list = new LinkedList<>();
+
+        for (int i = 0; i < 10; i++) {
+            list.addLast(i);
+        }
+
+        System.out.println(list);
+    }
+
+}

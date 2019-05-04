293239e04e13bf790f41699bdda4b1a03c1bc5ec
bianxinhuan <bianxinhuan@gmail.com>
2019-05-04 22:06:51 +0800

    6-7 二分搜索树的中序遍历和后序遍历

---
+++ src/06-Binary-Search-Tree/07-InOrder-and-PostOrder-Traverse-in-BST/src/Main.java
@@ -0,0 +1,33 @@
+/**
+ * -
+ *
+ * @author bianxinhuan
+ * @date 2019-05-03 22:14:40
+ */
+public class Main {
+    public static void main(String[] args) {
+        BST<Integer> bst = new BST<>();
+        int[] nums = {5, 3, 6, 8, 4, 2};
+
+        for (int i = 0; i < nums.length; i++) {
+            bst.add(nums[i]);
+        }
+
+        /////////////////
+        //      5      //
+        //    /   \    //
+        //   3    6    //
+        //  / \    \   //
+        // 2  4     8  //
+        /////////////////
+
+        bst.preOrder();
+        System.out.println();
+
+        bst.inOrder();
+        System.out.println();
+
+        bst.postOrder();
+        System.out.println();
+    }
+}

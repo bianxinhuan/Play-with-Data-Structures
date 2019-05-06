66c54324a9280c633f8f121deec3dfc660beead6
bianxinhuan <bianxinhuan@gmail.com>
2019-05-06 21:40:13 +0800

    6-9 二分搜索树前序遍历的非递归实现；leetcode 144. 二叉树的前序遍历

---
+++ src/06-Binary-Search-Tree/09-Non-Recursion-Preorder-Traverse-in-BST/src/Solution.java
@@ -0,0 +1,65 @@
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Stack;
+
+/**
+ * 给定一个二叉树，返回它的 前序 遍历。
+ * <p>
+ * /// Leetcode 144. Binary Tree Preorder Traversal
+ * /// https://leetcode.com/problems/binary-tree-preorder-traversal/description/
+ * ///
+ * /// 课程中在这里暂时没有介绍这个问题
+ * /// 该代码主要用于使用Leetcode上的问题测试我们的BST类
+ * /// 该测试主要测试前序遍历的非递归写法
+ *
+ * @author bianxinhuan
+ * @date 2019-05-06 21:28:42
+ */
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ * int val;
+ * TreeNode left;
+ * TreeNode right;
+ * TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+
+    public List<Integer> preorderTraversal(TreeNode root) {
+
+        List<Integer> res = new LinkedList<>();
+
+        if (root == null) {
+            return res;
+        }
+
+        Stack<TreeNode> stack = new Stack<>();
+        stack.push(root);
+
+        while (!stack.isEmpty()) {
+            TreeNode cur = stack.pop();
+            res.add(cur.val);
+
+            if (cur.right != null) {
+                stack.push(cur.right);
+            }
+            if (cur.left != null) {
+                stack.push(cur.left);
+            }
+        }
+
+        return res;
+    }
+}

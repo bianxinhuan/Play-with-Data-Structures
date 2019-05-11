aeed044aae0c7219623e20a4555576826b6abdac
bianxinhuan <bianxinhuan@gmail.com>
2019-05-11 11:20:06 +0800

    二叉树经典问题：另一种经典的非递归的前序遍历算法

--- src/06-Binary-Search-Tree/Optional-03-Binary-Tree-Classic-Nonrecursive-Traversal/Inorder/src/Solution2.java
+++ src/06-Binary-Search-Tree/Optional-03-Binary-Tree-Classic-Nonrecursive-Traversal/Inorder/src/Solution2.java
@@ -5,21 +5,15 @@ import java.util.Stack;
 /**
  * Leetcode 题目 94. 二叉树的中序遍历 https://leetcode.com/problems/binary-tree-inorder-traversal/solution/
  * <p>
- * // Classic Non-Recursive algorithm for inorder traversal
+ * // Another Classic Non-Recursive algorithm for inorder traversal
  * // Time Complexity: O(n), n is the node number in the tree
  * // Space Complexity: O(h), h is the height of the tree
  *
  * @author bianxinhuan
- * @date 2019-05-10 23:03:25
+ * @date 2019-05-11 09:49:00
  */
-public class Solution1 {
+public class Solution2 {
 
-    /**
-     * 给定一个二叉树，返回它的中序 遍历。
-     *
-     * @param root
-     * @return
-     */
     public List<Integer> inorderTraversal(TreeNode root) {
 
         List<Integer> res = new ArrayList<>();
@@ -30,17 +24,18 @@ public class Solution1 {
 
         Stack<TreeNode> stack = new Stack<>();
         TreeNode cur = root;
+
         while (cur != null || !stack.isEmpty()) {
-            while (cur != null) {
+            if (cur != null) {
                 stack.push(cur);
                 cur = cur.left;
+            } else {
+                cur = stack.pop();
+                res.add(cur.val);
+                cur = cur.right;
             }
-
-            cur = stack.pop();
-            res.add(cur.val);
-            cur = cur.right;
         }
-
         return res;
     }
+
 }

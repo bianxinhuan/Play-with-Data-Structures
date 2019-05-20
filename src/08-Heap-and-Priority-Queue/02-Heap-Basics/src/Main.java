7db897c75e847b0a6f34f4e48611947d7e7e6047
bianxinhuan <bianxinhuan@gmail.com>
2019-05-20 22:40:14 +0800

    8-2 堆的基础表示

---
+++ src/08-Heap-and-Priority-Queue/02-Heap-Basics/src/Main.java
@@ -0,0 +1,8 @@
+/**
+ * -
+ *
+ * @author bianxinhuan
+ * @date 2019-05-20 22:39:12
+ */
+public class Main {
+}
---
+++ src/08-Heap-and-Priority-Queue/02-Heap-Basics/src/MaxHeap.java
@@ -0,0 +1,67 @@
+/**
+ * 最大堆
+ *
+ * @author bianxinhuan
+ * @date 2019-05-20 22:28:26
+ */
+public class MaxHeap<E extends Comparable<E>> {
+
+    private Array<E> data;
+
+    public MaxHeap(int capacity) {
+        data = new Array<>(capacity);
+    }
+
+    public MaxHeap() {
+        data = new Array<>();
+    }
+
+    /**
+     * 返回堆中的元素个数
+     *
+     * @return
+     */
+    public int size() {
+        return data.getSize();
+    }
+
+    /**
+     * 返回一个布尔值，表示堆中是否为空
+     *
+     * @return
+     */
+    public boolean isEmpty() {
+        return data.isEmpty();
+    }
+
+    /**
+     * 返回完全二叉树的数组表示中，一个索引所表示的元素的父亲节点的索引
+     *
+     * @param index
+     * @return
+     */
+    private int parent(int index) {
+        if (index == 0) {
+            throw new IllegalArgumentException("index-0 doesn't have parent.");
+        }
+        return (index - 1) / 2;
+    }
+
+    /**
+     * 返回完全二叉树的数组表示中，一个索引所表示的元素的左孩子节点的索引
+     * @param index
+     * @return
+     */
+    private int leftChild(int index) {
+        return index * 2 + 1;
+    }
+
+    /**
+     * 返回完全二叉树的数组表示中，一个索引所表示的元素的右孩子节点的索引
+     * @param index
+     * @return
+     */
+    private int rightChild(int index) {
+        return index * 2 + 2;
+    }
+}

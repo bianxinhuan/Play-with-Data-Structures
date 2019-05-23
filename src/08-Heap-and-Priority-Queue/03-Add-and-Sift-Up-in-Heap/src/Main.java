89bff3b4eb2d084b6df6e4985f8be887a7015346
bianxinhuan <bianxinhuan@gmail.com>
2019-05-23 23:39:57 +0800

    8-3 向堆中添加元素和Sift Up

---
+++ src/08-Heap-and-Priority-Queue/03-Add-and-Sift-Up-in-Heap/src/Main.java
@@ -0,0 +1,9 @@
+/**
+ * -
+ *
+ * @author bianxinhuan
+ * @date 2019-05-20 22:39:12
+ */
+public class Main {
+
+}
---
+++ src/08-Heap-and-Priority-Queue/03-Add-and-Sift-Up-in-Heap/src/MaxHeap.java
@@ -0,0 +1,91 @@
+/**
+ * 最大堆
+ *
+ * @author bianxinhuan
+ * @date 2019-05-20 22:28:26
+ */
+public class MaxHeap<E extends Comparable<E>> {
+
+    private Array<E> data;
+
+    public MaxHeap(int capacity) {
+        data = new Array<>(capacity);
+    }
+
+    public MaxHeap() {
+        data = new Array<>();
+    }
+
+    /**
+     * 返回堆中的元素个数
+     *
+     * @return
+     */
+    public int size() {
+        return data.getSize();
+    }
+
+    /**
+     * 返回一个布尔值，表示堆中是否为空
+     *
+     * @return
+     */
+    public boolean isEmpty() {
+        return data.isEmpty();
+    }
+
+    /**
+     * 返回完全二叉树的数组表示中，一个索引所表示的元素的父亲节点的索引
+     *
+     * @param index
+     * @return
+     */
+    private int parent(int index) {
+        if (index == 0) {
+            throw new IllegalArgumentException("index-0 doesn't have parent.");
+        }
+        return (index - 1) / 2;
+    }
+
+    /**
+     * 返回完全二叉树的数组表示中，一个索引所表示的元素的左孩子节点的索引
+     *
+     * @param index
+     * @return
+     */
+    private int leftChild(int index) {
+        return index * 2 + 1;
+    }
+
+    /**
+     * 返回完全二叉树的数组表示中，一个索引所表示的元素的右孩子节点的索引
+     *
+     * @param index
+     * @return
+     */
+    private int rightChild(int index) {
+        return index * 2 + 2;
+    }
+
+    /**
+     * 向堆中添加元素
+     *
+     * @param e
+     */
+    public void add(E e) {
+        data.addLast(e);
+        siftUp(data.getSize() - 1);
+    }
+
+    /**
+     * 堆中的元素上浮
+     *
+     * @param k 元素的索引
+     */
+    private void siftUp(int k) {
+        while (k > 0 && data.get(k).compareTo(data.get(parent(k))) > 0) {
+            data.swap(k, parent(k));
+            k = parent(k);
+        }
+    }
+}

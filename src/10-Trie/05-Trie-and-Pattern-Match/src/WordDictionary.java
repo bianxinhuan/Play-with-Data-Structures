cb21dbeb97aab3d0d5adc5ac256a21cf365e3231
bianxinhuan <bianxinhuan@gmail.com>
2019-06-03 21:26:05 +0800

    10-5 Trie字典树和简单的模式匹配

---
+++ src/10-Trie/05-Trie-and-Pattern-Match/src/WordDictionary.java
@@ -0,0 +1,88 @@
+import java.util.TreeMap;
+
+/**
+ * Leetcode 211. Add and Search Word - Data structure design 添加与搜索单词 - 数据结构设计
+ * https://leetcode.com/problems/add-and-search-word-data-structure-design/description/
+ *
+ * @author bianxinhuan
+ * @date 2019-06-03 13:30:49
+ */
+public class WordDictionary {
+
+    private class Node {
+        public boolean isWord;
+        public TreeMap<Character, Node> next;
+
+        public Node(boolean isWord) {
+            this.isWord = isWord;
+            next = new TreeMap<>();
+        }
+
+        public Node() {
+            this(false);
+        }
+    }
+
+    private Node root;
+
+    /**
+     * Initialize your data structure here.
+     */
+    public WordDictionary() {
+        root = new Node();
+    }
+
+    /**
+     * Adds a word into the data structure.
+     */
+    public void addWord(String word) {
+
+        Node cur = root;
+        for (int i = 0; i < word.length(); i++) {
+            char c = word.charAt(i);
+            if (cur.next.get(c) == null) {
+                cur.next.put(c, new Node());
+            }
+            cur = cur.next.get(c);
+        }
+        cur.isWord = true;
+    }
+
+    /**
+     * Returns if the word is in the data structure. A word could contain the dot character '.' to represent any one letter.
+     */
+    public boolean search(String word) {
+        return match(root, word, 0);
+    }
+
+    private boolean match(Node node, String word, int index) {
+
+        if (index == word.length()) {
+            return node.isWord;
+        }
+
+        char c = word.charAt(index);
+
+        // 匹配正常字符
+        if (c != '.') {
+            if (node.next.get(c) == null) {
+                return false;
+            }
+            return match(node.next.get(c), word, index + 1);
+        } else { // 匹配通配符'.'，遍历所有子节点
+            for (char nextChar : node.next.keySet()) {
+                if (match(node.next.get(nextChar), word, index + 1)) {
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+}
+
+/**
+ * Your WordDictionary object will be instantiated and called as such:
+ * WordDictionary obj = new WordDictionary();
+ * obj.addWord(word);
+ * boolean param_2 = obj.search(word);
+ */

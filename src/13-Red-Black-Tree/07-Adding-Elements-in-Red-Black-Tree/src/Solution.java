478b33ab8197f2fbe50e972eae0c749d00b46cdf
bianxinhuan <bianxinhuan@gmail.com>
2019-06-17 23:10:31 +0800

    13-7 红黑树中添加新元素

---
+++ src/13-Red-Black-Tree/07-Adding-Elements-in-Red-Black-Tree/src/Solution.java
@@ -0,0 +1,298 @@
+/**
+ * Leetcode 804. Unique Morse Code Words
+ * https://leetcode.com/problems/unique-morse-code-words/description/
+ * <p>
+ * 课程中在这里暂时没有介绍这个问题
+ * 该代码主要用于使用Leetcode上的问题测试我们的BST类
+ *
+ * @author bianxinhuan
+ * @date 2019-06-17 22:20:41
+ */
+public class Solution {
+
+    private class RBTree<K extends Comparable<K>, V> {
+
+        public static final boolean RED = true;
+        public static final boolean BLACK = false;
+
+        private class Node {
+            public K key;
+            public V value;
+            public Node left, right;
+            public boolean color;
+
+            public Node(K key, V value) {
+                this.key = key;
+                this.value = value;
+                left = null;
+                right = null;
+                color = RED;
+            }
+        }
+
+        private Node root;
+        private int size;
+
+        public RBTree() {
+            root = null;
+            size = 0;
+        }
+
+        /**
+         * 判断节点node的颜色
+         *
+         * @param node
+         * @return
+         */
+        private boolean isRed(Node node) {
+            if (node == null) {
+                return BLACK;
+            }
+            return node.color;
+        }
+
+        //   node                     x
+        //  /   \     左旋转         /  \
+        // T1   x   --------->   node   T3
+        //     / \              /   \
+        //    T2 T3            T1   T2
+        private Node leftRotate(Node node) {
+
+            Node x = node.right;
+
+            // 左旋转
+            node.right = x.left;
+            x.left = node;
+
+            x.color = node.color;
+            node.color = RED;
+
+            return x;
+        }
+
+        //     node                   x
+        //    /   \     右旋转       /  \
+        //   x    T2   ------->   y   node
+        //  / \                       /  \
+        // y  T1                     T1  T2
+        private Node rightRotate(Node node) {
+
+            Node x = node.left;
+
+            // 右旋转
+            node.left = x.right;
+            x.right = node;
+
+            x.color = node.color;
+            node.color = RED;
+
+            return x;
+        }
+
+        // 颜色翻转
+        private void flipColors(Node node) {
+
+            node.color = RED;
+            node.left.color = BLACK;
+            node.right.color = BLACK;
+        }
+
+        /**
+         * 向二分搜索树中添加新的元素(key, value)
+         *
+         * @param key
+         * @param value
+         */
+        public void add(K key, V value) {
+            root = add(root, key, value);
+            // 最终根节点为黑色节点
+            root.color = BLACK;
+        }
+
+        /**
+         * 向以node为根的二分搜索树中插入元素(key, value)，递归算法
+         * 返回插入新节点后二分搜索树的根
+         *
+         * @param node
+         * @param key
+         * @param value
+         * @return
+         */
+        private Node add(Node node, K key, V value) {
+
+            if (node == null) {
+                size++;
+                // 默认插入红色节点
+                return new Node(key, value);
+            }
+
+            if (key.compareTo(node.key) < 0) {
+                node.left = add(node.left, key, value);
+            } else if (key.compareTo(node.key) > 0) {
+                node.right = add(node.right, key, value);
+            } else { //  key.compareTo(node.key) == 0
+                node.value = value;
+            }
+
+            if (isRed(node.right) && !isRed(node.left)) {
+                node = leftRotate(node);
+            }
+
+            if (isRed(node.left) && isRed(node.left.left)) {
+                node = rightRotate(node);
+            }
+
+            if (isRed(node.left) && isRed(node.right)) {
+                flipColors(node);
+            }
+
+            return node;
+        }
+
+        /**
+         * 返回以node为根节点的二分搜索树中，key所在的节点
+         *
+         * @param node
+         * @param key
+         * @return
+         */
+        private Node getNode(Node node, K key) {
+
+            if (node == null) {
+                return null;
+            }
+
+            if (key.equals(node.key)) {
+                return node;
+            } else if (key.compareTo(node.key) < 0) {
+                return getNode(node.left, key);
+            } else { // if(key.compareTo(node.key) > 0
+                return getNode(node.right, key);
+            }
+        }
+
+        /**
+         * 返回以node为根的二分搜索树的最小值所在的节点
+         *
+         * @param node
+         * @return
+         */
+        private Node minimum(Node node) {
+
+            if (node.left == null) {
+                return node;
+            }
+            return minimum(node.left);
+        }
+
+        public V remove(K key) {
+
+            Node node = getNode(root, key);
+
+            if (node != null) {
+                root = remove(root, key);
+                return node.value;
+            }
+
+            return null;
+        }
+
+        private Node remove(Node node, K key) {
+
+            if (node == null) {
+                return null;
+            }
+
+            Node retNode;
+            if (key.compareTo(node.key) < 0) {
+                node.left = remove(node.left, key);
+                // return node;
+                retNode = node;
+            } else if (key.compareTo(node.key) > 0) {
+                node.right = remove(node.right, key);
+                // return node;
+                retNode = node;
+            } else { // key.compareTo(node.key) == 0
+
+                // 待删除节点左子树为空的情况
+                if (node.left == null) {
+                    Node rightNode = node.right;
+                    node.right = null;
+                    size--;
+                    // return rightNode;
+                    retNode = rightNode;
+
+                    // 待删除节点右子树为空的情况
+                } else if (node.right == null) {
+                    Node leftNode = node.left;
+                    node.left = null;
+                    size--;
+                    // return leftNode;
+                    retNode = leftNode;
+
+                    // 待删除节点左右子树均不为空的情况
+                } else {
+                    // 找到比待删除节点大的最小节点, 即待删除节点右子树的最小节点
+                    // 用这个节点顶替待删除节点的位置
+                    Node successor = minimum(node.right);
+                    // successor.right = removeMin(node.right);
+                    successor.right = remove(node.right, successor.key);
+                    successor.left = node.left;
+
+                    node.left = node.right = null;
+
+                    // return successor;
+                    retNode = successor;
+                }
+            }
+
+            if (retNode == null) {
+                return null;
+            }
+
+            return retNode;
+        }
+
+        public boolean contains(K key) {
+            return getNode(root, key) != null;
+        }
+
+        public V get(K key) {
+            Node node = getNode(root, key);
+            return node == null ? null : node.value;
+        }
+
+        public void set(K key, V newValue) {
+            Node node = getNode(root, key);
+            if (node == null) {
+                throw new IllegalArgumentException(key + " does't exist!");
+            }
+            node.value = newValue;
+        }
+
+        public int getSize() {
+            return size;
+        }
+
+        public boolean isEmpty() {
+            return size == 0;
+        }
+    }
+
+    public int uniqueMorseRepresentations(String[] words) {
+
+        String[] codes = {".-", "-...", "-.-.", "-..", ".", "..-.", "--.", "....", "..", ".---", "-.-", ".-..", "--", "-.", "---", ".--.", "--.-", ".-.", "...", "-", "..-", "...-", ".--", "-..-", "-.--", "--.."};
+        RBTree<String, Object> set = new RBTree<>();
+        for (String word : words) {
+            StringBuilder res = new StringBuilder();
+            for (int i = 0; i < word.length(); i++) {
+                res.append(codes[word.charAt(i) - 'a']);
+            }
+
+            set.add(res.toString(), null);
+        }
+
+        return set.getSize();
+    }
+
+}
